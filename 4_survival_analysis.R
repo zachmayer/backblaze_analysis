f05d2d24fd815da6ccc06f07594f893e51231dfb
Zachary Deane-Mayer <zach.mayer@gmail.com>
2021-10-19 17:07:57 -0400

    ideas

--- 4_survival_analysis.R
+++ 4_survival_analysis.R
@@ -8,8 +8,7 @@ library(data.table)
 library(kit)
 library(survival)
 library(ggthemes)
-library(prodlim)
-
+library(muhaz)
 days_to_year <- 365.2425
 custom_palette <- c(
   "#1f78b4", "#ff7f00", "#6a3d9a", "#33a02c", "#e31a1c", "#b15928",
@@ -42,88 +41,56 @@ drive_dates[is.finite(first_fail), max_date := first_fail]
 drive_dates[,days := as.integer(max_date - min_date)]
 drive_dates[,model := factor(model)]
 
-# Fit a cox model
-ref_level <- drive_dates[,list(days=sum(days), .N), by='model'][which.max(days), as.character(model)]
+# Choose the "reference" class based on the drive with the most days
+ref_level <- drive_dates[,list(days=max(days), .N), by='model'][which.max(days), as.character(model)]
 drive_dates[,model := factor(model)]
 drive_dates[,model := relevel(model, ref=ref_level)]
-cox_model <- drive_dates[,coxph(Surv(time=days, failed) ~ 1 + model)]
-cox_model_hazard <- basehaz(cox_model)
+cox_model <- drive_dates[,coxph(Surv(time=days, failed) ~ 1 + model, x=T)]
+
+# Extract baseline hazard
+cox_model_curve <- survfit(cox_model)
+cox_model_hazard <- data.table(
+  time = cox_model_curve[['time']],
+  n.risk = cox_model_curve[['n.risk']],
+  n.event = cox_model_curve[['n.event']],
+  n.censor = cox_model_curve[['n.censor']],
+  surv = cox_model_curve[['surv']],
+  cumhaz = cox_model_curve[['cumhaz']],
+  std.err = cox_model_curve[['std.err']],
+  n.risk = cox_model_curve[['n.risk']]
+)
+setorderv(cox_model_hazard, 'time')
 
 # Extract cox model coefficients
-cf <- coef(summary(cox_model))
-cf <- data.table(model=row.names(cf), cf)
-cf[,upper_ci := coef + `se(coef)`]
-cf[, model := gsub('model', '', model,fixed=T)]
-cf <- cf[order(upper_ci),]
-head(cf, 25)
-
-# Fit a cox model
 cf <- summary(cox_model)
 cf <- data.table(model=row.names(cf$conf.int), cf$conf.int)
-cf <- cf[order(`upper .95`),]
-head(cf[is.finite(`upper .95`),], 50)
+cf[,model := gsub('model', '', model, fixed=T)]
 
-# Calculate survival days for the 99th percentile for each drive
-surv_days_99 <- quantile(surv_model, .01)
-surv_days_99 <- data.table(
-  model = names(surv_days_99),
-  rbindlist(lapply(surv_days_99, function(x) data.table(x)), use.names = T, fill=T)
-)
-setnames(
-  surv_days_99, 
-  c('quantile', 'lower', 'upper'),
-  c('survial_days_99_percent', 'survial_days_99_percent_lower', 'survial_days_99_percent_upper')
-  )
+# Calculate survival rate at 10 years
+surv_pct_at_years <- summary(cox_model_curve, times=10 * days_to_year, extend=TRUE)
 
-# Calculate survival rate at 5 years
-surv_pct_at_years <- summary(
-  surv_model, times=1 * days_to_year, conf.int=.9999, extend=TRUE, max.tables=999999,
-  intervals=T
-  )$table
-surv_pct_at_years <- data.table(
-  model = names(surv_pct_at_years),
-  rbindlist(lapply(surv_pct_at_years, function(x) data.table(x)), use.names = T, fill=T)
-)
-surv_pct_at_years <- data.table(
-  model = surv_pct_at_years$model,
-  survival_pct_1_year = surv_pct_at_years$surv,
-  survival_pct_1_year_lower = surv_pct_at_years$lower,
-  survival_pct_1_year_upper = surv_pct_at_years$upper
-)
-
-# Combine survival statistics
-dat <- merge(surv_days_99, surv_pct_at_years, by='model', all=T)
-dat[,model := gsub('model=', '', model, fixed=T)]
+# Add 99% / 5 years to table
+cf[,surv_10_years_lower := (1 - `upper .95`) * surv_pct_at_years$surv]
+cf[,surv_10_years := (1 - `exp(coef)`) * surv_pct_at_years$surv]
+cf[,surv_10_years_upper := (1 - `lower .95`) * surv_pct_at_years$surv]
 
 # Calculate drive stats and combine with summary stats
-drive_summary <- drive_dates[,list(
+dat <- drive_dates[,list(
   n_unique=length(funique(serial_number)),
   drive_days = sum(days),
   naive_age_med = as.numeric(median(days)),
   naive_age_mean = as.numeric(mean(days)),
   failed = sum(failed)
 ), by='model']
-drive_summary[model %in% model_list,]
-dat <- merge(drive_summary, dat, by='model', all=T)
-
-# Add drive TB
+dat[,model := as.character(model)]
+dat <- merge(dat, cf, by='model', all=F)
 dat <- merge(dat, capacity_map, by='model', all=F)
 
 # Show data
-dat <- dat[, list(
-  model, size, n_unique, drive_days, failed, survial_days_99_percent, 
-  survival_pct_1_year_lower, pois_ci, naive_age_med, naive_age_mean)]
-
-# Best by 1-year rate
-dat[!is.na(survival_pct_1_year_lower),][order(-survival_pct_1_year_lower),]
-
-# Best by percentile
-dat[!is.na(survial_days_99_percent),][order(-survial_days_99_percent),]
-
-# Best by pois
-dat[!is.na(pois_ci),][order(-pois_ci),]
-
-
+dat <- dat[order(-surv_10_years_lower), list(
+  model, size, n_unique, drive_days, failed, 
+  surv_10_years_lower, surv_10_years, surv_10_years_upper, naive_age_med)]
+dat[!is.na(surv_10_years_lower),]
 
 # Plot survival curves for the best drives by size
 plot_dat <- drive_dates[model %in% model_list,]
