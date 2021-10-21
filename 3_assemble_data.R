06314f2f1be39b29076a14e9dfe3096a80843596
Zach Mayer <581590+zachmayer@users.noreply.github.com>
2021-10-20 21:07:53 -0400

    move to a new r script (#2)
    * move to a new r script
    
    * worl
    
    * fix surve
    
    * LOTS OF NEW IDEAS.  ADD ONE DEATH PER GROUP
    
    * fffff
    
    * ideas
    
    * ideas
    
    * hmm:
    
    * new analysis
    
    * reformat
    
    * edit
    
    * edit
    
    * edit
    
    * edit
    
    * edit
    
    * edit
    
    * edit

--- 3_assemble_data.R
+++ 3_assemble_data.R
@@ -1,50 +1,65 @@
 # Setup
+stop()
+rm(list = ls(all=T))
+gc(reset = T)
 library(pbapply)
 library(data.table)
+library(compiler)
+library(fastmatch)
+library(kit)
 
 # Load the data
+# NOTE: 54+ drives have more than one "failure" day
+# I *think* this means that the drive failure was fixed, and the drive was re-deployed
+# HOWEVER; I don't want to fix my own drives, so I am going to consider any "failure"
+# to be game ove
 data_dir <- 'data/'
 all_files <- list.files(data_dir)
-all_data <- pblapply(
+all_data <- pblapply(  # Takes ~18 minutes
   all_files,
   function(x){
     out <- fread(
       paste0(data_dir, x), 
       select=c('model', 'serial_number', 'failure', 'capacity_bytes'),
-      colClasses=c(capacity_bytes='character') # We lose a tiny bit of precision, but who cares
+      colClasses=c(capacity_bytes='numeric') # We lose a tiny bit of precision, but who cares
     )
-    out[,filename := x]  # TODO: check which files have capacities < 0
+    out[,filename := x]
   }
 )
-all_data <- rbindlist(all_data)
+all_data <- rbindlist(all_data, use.names=TRUE, fill=TRUE)
+all_data[,capacity_bytes := as.integer(capacity_bytes)]
 
-# Look at bad capacities
-all_data[capacity_bytes == "-1", sort(unique(filename))]
-all_data[capacity_bytes == "-9116022715867848704", sort(unique(filename))]
+# Filename to date
+file_to_date <- cmpfun(function(x){
+  x_unique <- sort(funique(x))
+  x_map <- fmatch(x, x_unique)
+  x_date <- as.Date(gsub('.csv', '', x_unique, fixed=T))
+  return(x_date[x_map])
+})
+all_data[,date := file_to_date(filename)]
 
-# Aggregate to counts by row (lots dupes so we can compress the data a lot here)
-keys <- c('model', 'serial_number', 'failure', 'capacity_bytes')
-setkeyv(all_data, keys)
-all_data <- all_data[,list(.N), by=keys]
+# Data quality checks=: bad capacity
+all_data[,sort(funique(capacity_bytes))]
+all_data[capacity_bytes == "-1", summary(date)]
+all_data[capacity_bytes == "-9116022715867848704", summary(date)]
+gc(reset=T)
 
-# Data quality checks
-all_data[,sort(unique(capacity_bytes))]
+# Capacity map
+capacity_map <- all_data[,list(model, capacity_bytes)]
+capacity_map[,capacity_bytes := as.numeric(capacity_bytes)]
+setkeyv(capacity_map, 'capacity_bytes')
+gc(reset=T)
+capacity_map <- capacity_map[which(capacity_bytes > 0),]  # HDs can't have negative capacity
+capacity_map <- capacity_map[which(capacity_bytes < 1e+15),]  # 1 PetaByte drives don't exist yet
+setkeyv(capacity_map, 'model')
+capacity_map <- capacity_map[,list(capacity_bytes=max(capacity_bytes)), by='model']
+fwrite(capacity_map, 'capacity_map.csv')
+gc(reset=T)
 
-summary(all_data[capacity_bytes == "-1",])
-all_data[capacity_bytes == "-1",]
-all_data[capacity_bytes == "-9116022715867848704",]
-
-# Map models to capacity
-all_data[,max_N := (N==max(N)), by='model']
-capacity_map <- all_data[which(max_N),list(capacity_bytes=max(as.numeric(capacity_bytes))),by='model']
-all_data[,max_N := NULL]
-all_data[,capacity_bytes := NULL]
-
-N <- nrow(all_data)
-all_data <- merge(all_data, capacity_map, by='model', all.x=T, all.y=F)
-stopifnot(nrow(all_data) == N)
-
-# Save data
-setkeyv(all_data, keys)
-fwrite(all_data, 'all_data.csv')
-# Now you can delete the "data" dir to save a bunch of GB of data
+# Calculate dates by serial number
+setkeyv(all_data, c('model', 'serial_number'))
+drive_dates <- all_data[, list(
+  min_date = min(date),
+  max_date = max(date),
+  first_fail = min(date[failure==1])), by=c('model', 'serial_number')]
+fwrite(drive_dates, 'drive_dates.csv')
---
+++ 4_survival_analysis.R
@@ -0,0 +1,102 @@
+# Setup
+# TODO: TRY A PROPORTIAL HAZARD MODEL — the 14 TB DRIVES LOOK GREAT BUT AREN'T YET 1 YEAR OLD
+# TODO: Plot best drive by size
+stop()
+rm(list = ls(all=T))
+gc(reset = T)
+library(data.table)
+library(kit)
+library(survival)
+library(ggthemes)
+library(survminer)
+days_to_year <- 365.2425
+
+# Load data
+capacity_map <- fread('capacity_map.csv')
+drive_dates <- fread('drive_dates.csv')
+
+# Format HD sizes nicely
+format_hd_size <- function(x, ...){
+  utils:::format.object_size(x, ..., standard='SI', digits=0)
+}
+capacity_map[capacity_bytes <  1e+12, size := format_hd_size(capacity_bytes, 'GB')]
+capacity_map[capacity_bytes >= 1e+12, size := format_hd_size(capacity_bytes, 'TB')]
+
+capacity_order <- capacity_map[,list(capacity_bytes=max(capacity_bytes)), by='size']
+capacity_order <- capacity_order[order(capacity_bytes),]
+capacity_map[,size := factor(size, levels=capacity_order[,size], ordered=T)]
+fwrite(capacity_map, 'capacity_map_clean.csv')
+fwrite(capacity_order, 'capacity_order.csv')
+
+# Computer drive failure, and time to failure or time to censoring
+drive_dates[,failed := as.integer(is.finite(first_fail))]
+drive_dates[is.finite(first_fail), max_date := first_fail]
+drive_dates[,days := as.integer(max_date - min_date)]
+drive_dates[,model := factor(model)]
+
+# Choose the "reference" class based on the drive with the most days
+ref_level <- drive_dates[,list(days=max(days), .N, sum_days=sum(days)), by='model'][which.max(days), as.character(model)]
+ref_level <- 'HGST HMS5C4040BLE640'  # Reliable 4TB drive with lots of drives and drive days
+drive_dates[,model := factor(model)]
+drive_dates[,model := relevel(model, ref=ref_level)]
+fwrite(drive_dates, 'drive_dates_clean.csv')
+
+# Fit the cox model
+cox_model <- drive_dates[,coxph(Surv(time=days, failed) ~ 1 + model, x=T)]
+
+# Extract cox model coefficients
+# These coefficients are "hazard ratios".  Lower hazard is better
+cf <- summary(cox_model)
+cf <- data.table(
+  model=row.names(coef(cf)), 
+  coef(cf), 
+  cf$conf.int[,c('lower .95', 'upper .95')]
+  )
+stopifnot(sum(duplicated(names(cf)))==0)
+cf[,model := gsub('model', '', model, fixed=T)]
+cf <- cf[order(`upper .95`, coef),]
+
+# Calculate median survival and 5 year survival per model
+CONF_LEVEL = .95
+i <- 0
+pb = txtProgressBar(min = 0, max = length(cf[['model']]), initial = 0, style=3) 
+for(m in cf[['model']]){
+  cox_surv_curve <- survfit(cox_model, conf.int=CONF_LEVEL, newdata=data.table(model=m), conf.type="logit")
+  quantile_surv <- quantile(cox_surv_curve, .01)
+  surv_5_year <- summary(cox_surv_curve, conf.int=CONF_LEVEL, 1 * days_to_year)
+
+  cf[model == m, surv_days_99pct := quantile_surv[["lower"]]]
+  cf[model == m, surv_5yr_lower := surv_5_year$lower]
+  cf[model == m, surv_5yr := surv_5_year$surv]
+  cf[model == m, surv_5yr_upper := surv_5_year$upper]
+  
+  i <- i + 1
+  setTxtProgressBar(pb,i)
+}
+close(pb)
+
+# Calculate drive stats and combine with summary stats
+dat <- drive_dates[,list(
+  n_unique=length(funique(serial_number)),
+  drive_days = sum(days),
+  naive_age_med = as.numeric(median(days)),
+  naive_age_mean = as.numeric(mean(days)),
+  failed = sum(failed)
+), by='model']
+dat[,model := as.character(model)]
+dat <- merge(dat, cf, by='model', all=F)
+dat <- merge(dat, capacity_map, by='model', all=F)
+
+# Order data and select columns
+dat <- dat[order(
+  -surv_5yr_lower, -surv_5yr, surv_5yr_upper,
+  -surv_days_99pct,
+  -drive_days, -n_unique, failed, -size),]
+dat <- dat[, list(
+  model, size, n_unique,
+  drive_days, failed, surv_days_99pct,
+  surv_5yr_lower, surv_5yr, surv_5yr_upper)]
+
+# Save data for the write up
+fwrite(dat, 'survival_results.csv')
+saveRDS(cox_model, 'cox_model.rds')
