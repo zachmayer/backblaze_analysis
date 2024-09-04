613aa4b4b6ab7f8b7e0f6cb6832de6ab611aa135
Zach Deane-Mayer <zach.mayer@gmail.com>
2024-09-04 11:29:42 -0400

    less so

--- code/process_csv_files.R
+++ code/process_csv_files.R
@@ -10,19 +10,11 @@
 parser <- argparser::arg_parser("Process CSV files and combine them")
 parser <- argparser::add_argument(parser, "--input", help="Input directory containing CSV files", type="character")
 parser <- argparser::add_argument(parser, "--output", help="Output CSV file", type="character")
-parser <- argparser::add_argument(parser, "--key",
-                                  help="Comma-separated list of key columns",
-                                  default="serial_number,model,capacity_bytes")
-parser <- argparser::add_argument(parser, "--select",
-                                  help="Comma-separated list of extra columns to select",
-                                  default="date,failure")
 parser <- argparser::add_argument(parser, "--verbose",
                                   help="Print extra output",
                                   flag=TRUE)
 
 args <- argparser::parse_args(parser)
-args$key <- unlist(strsplit(args$key, ","))
-args$select <- unlist(strsplit(args$select, ","))
 if (args$verbose) print(dput(args))
 
 # Validate required arguments
@@ -31,31 +23,32 @@ if (is.na(args$input) || is.na(args$output)) {
 }
 
 # Process CSV files
+keys <- c("serial_number", "model", "capacity_bytes")
 apply_fun = lapply
 if (args$verbose) apply_fun = pbapply::pblapply
 dt <- data.table::rbindlist(
   apply_fun(
     list.files(path = args$input, pattern = "*.csv", full.names = TRUE),
     data.table::fread,
-    select = unique(c(args$key, args$select)),
+    select = c("date", keys, "failure"),
     integer64 = "numeric",  # Loose a little tiny precision off drive capacity
     encoding = "UTF-8",
     blank.lines.skip = TRUE,
     showProgress = args$verbose
   )
 )
-data.table::setkeyv(dt, args$key)
+data.table::setkeyv(dt, keys)
 if (args$verbose) cat("Processing", args$input, "successful.\n")
 
 # Calculate summary statistics
-summary_dt <- dt[, .(
+summary_dt <- dt[, list(
   min_date = min(date),
   max_date = max(date),
   first_fail = suppressWarnings(min(date[failure == 1L], na.rm = TRUE))
-), by = args$key]
+), by = keys]
 
 # Sort the final output
-data.table::setorderv(summary_dt, args$key)
+data.table::setorderv(summary_dt, keys)
 
 # Write the final output
 data.table::fwrite(summary_dt, args$output, sep = ",", quote = TRUE, encoding = "UTF-8")
