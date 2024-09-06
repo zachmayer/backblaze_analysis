36129af6c57760522263602bf626d25ac531f40a
Zach Deane-Mayer <zach.mayer@gmail.com>
2024-09-06 09:53:53 -0400

    remove old code

--- code/helpers.R
+++
@@ -1,9 +0,0 @@
-# Days in a year
-days_to_year <- 365.2425
-
-# Custom color custom_palette
-custom_palette <- c(
-  "#1f78b4", "#ff7f00", "#6a3d9a", "#33a02c", "#e31a1c", "#b15928",
-  "#a6cee3", "#fdbf6f", "#cab2d6", "#b2df8a", "#fb9a99", "black",
-  "grey1", "grey10"
-)
